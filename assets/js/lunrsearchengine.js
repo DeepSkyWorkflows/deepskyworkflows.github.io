c1c7fc3d0f8ce47104db5d33117c1c6b6c1acda9
Jeremy Likness <jeremy@jeremylikness.com>
2022-11-29 17:32:58 -0800

    fix search bug

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -4,13 +4,13 @@ sitemap: false
 ---
 
 {% assign counter = 0 %}
-var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or page.url contains 'assets' or page.url contains 'category' or page.url contains 'tag' %}{% else %}{
+var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or page.url contains 'assets' or page.url contains 'category' or page.url contains 'tag' or page.url contains '/_' %}{% else %}{
     "id": {{ counter }},
     "url": "{{ site.url }}{{site.baseurl}}{{ page.url }}",
     "title": "{{ page.title }}",
     "body": "{{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
     }, {% endif %}{% endfor %}
-    {% for page in site.gallery %}{% if page.url contains '.xml' or page.url contains 'assets' or page.url contains 'category' or page.url contains 'tag' %}{% else %}{
+    {% for page in site.gallery %}{% if page.url contains '.xml' or page.url contains 'assets' or page.url contains 'category' or page.url contains 'tag' or page.url contains '/_'  %}{% else %}{
         "id": {{ counter }},
         "url": "{{ site.url }}{{site.baseurl}}{{ page.url }}",
         "title": "{{ page.title }}",
